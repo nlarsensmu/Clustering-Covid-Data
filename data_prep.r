f893558a4d0895bf2471841aa486992d4d429c75
nlarsensmu <nlarsen@smu.edu>
2022-03-25 11:50:20 -0500

    Add Nick changes.

--- data_prep.r
+++ data_prep.r
@@ -9,36 +9,33 @@ library("ggpubr")
 
 
 
-covid_data_2021_by_month <- read_csv("./data/Query1.csv")
-data_final <- read_csv("./data/2020_2021_deaths_county_month.csv")
+data_final <- read_csv("./data/Query1.csv")
 
 # Normalize population
 data_final$original_total_pop <- data_final$total_pop
+
 data_final <- data_final %>% mutate_at(c("total_pop"), ~(scale(.) %>% as.vector))
 
-# get the new cases/deaths feature
-data_offset <- filter(data_final, year == 2020 | (year == 2021 & month <= 11))
-data_final <- filter(data_final, (year == 2020 & month >= 2) | year == 2021)
-
-data_final$new_cases <- data_final$confirmed_cases - data_offset$confirmed_cases
-data_final$new_deaths <- data_final$deaths - data_offset$deaths
-
 # Get the rent burden feature
+
+
 # Break up into Severe burden, rent burden and no burden
 
-data_final$severe_burden = data_final$rent_over_50_percent / data_final$original_total_pop
+data_final$severe_burden = data_final$rent_over_50_percent / data_final$households
 
 data_final$rent_burden = (data_final$rent_40_to_50_percent +
                             data_final$rent_35_to_40_percent +
-                            data_final$rent_30_to_35_percent) / data_final$original_total_pop
+                            data_final$rent_30_to_35_percent) / data_final$households
 data_final$no_rent_burden = (data_final$rent_25_to_30_percent +
                                data_final$rent_20_to_25_percent +
                                data_final$rent_15_to_20_percent + 
                                data_final$rent_10_to_15_percent +
                                data_final$rent_10_to_15_percent +
-                               data_final$rent_under_10_percent) /
-  data_final$original_total_pop
+                               data_final$rent_under_10_percent) / data_final$households
 
+data_final$test= data_final$severe_burden + data_final$rent_burden + data_final$no_rent_burden + (data_final$rent_burden_not_computed/data_final$households)
+
+data_final %>% select(test) %>% summary()
 
 # Get cases/deaths per 1000
 data_final$confirmed_cases_per1000 = data_final$confirmed_cases / 
@@ -65,7 +62,8 @@ temp <- data_final %>% select(total_pop, original_total_pop, no_rent_burden, ren
                       deaths_per1000, confirmed_cases_per1000,
                       white_pop_per, black_pop_per, asian_pop_per, hispanic_pop_per,
                       amerindian_pop_per, other_race_pop_per, two_or_more_races_pop_per,
-                      not_hispanic_pop_per, commuters_by_public_transportation)
+                      not_hispanic_pop_per, commuters_by_public_transportation,
+                      county_name, state, county_fips_code)
 temp
 temp %>% write.csv(file = "datatable.csv")
 
@@ -95,3 +93,7 @@ for (i in 1:length(col_names)){
 }
 plots
 plots[3]
+
+temp %>% select(no_rent_burden, rent_burden, severe_burden)
+
+summary(temp)
