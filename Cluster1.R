bedbf5d22ad951ae3690ee0ff94b8c735fb2d736
Steven <stevenjohnlarsen@gmail.com>
2022-03-27 19:32:18 -0500

    I think I am done with cluster 1

--- Cluster1.R
+++ Cluster1.R
@@ -1,5 +1,8 @@
 library("tidyverse")
 library("ggplot2")
+library("dplyr")
+library("tidyr")
+
 
 data <- read_csv("datatable.csv")
 colnames(data)
@@ -37,75 +40,47 @@ data_final <- data_final %>% mutate_at(c("other_race_pop_per"), ~(scale(.) %>% a
 data_final <- data_final %>% mutate_at(c("two_or_more_races_pop_per"), ~(scale(.) %>% as.vector))
 data_final <- data_final %>% mutate_at(c("not_hispanic_pop_per"), ~(scale(.) %>% as.vector))
 data_final <- data_final %>% mutate_at(c("commuters_by_public_transportation"), ~(scale(.) %>% as.vector))
-km <- kmeans(data_final, centers = 4)
-km$tot.withinss
 
-kms <- rep(NA, 14)
-sse_scores <- rep(NA, 14)
+#we are going to look at the different clusters 
+number_of_clusters <- 40
+sse_scores <- rep(NA, number_of_clusters)
 
-#Distance functions
-euc_dist <- function(p1,p2) {
-  sqrt(sum((p1 - p2) ^ 2))
-}
-man_dist <- function(p1,p2) {
- sum(abs(p1 - p2))
-}
-km <- kmeans(data_final, centers = 5, iter.max =30)
-km$cluster
-#similarity scores
-#returns a single row of the coesion of 
-graph_coesion <- function(data, clusters, distance) {
-  temp_data <- data
-  temp_data$cluster <- clusters
-  number_of_clusters <- max(clusters)
-  similarities <- rep(0, number_of_clusters)
-  for (i in 1:number_of_clusters) {
-    cluster_data <- filter(temp_data, cluster == i)
-    cluster_length <- dim(cluster_data)[1]
-    sum = 0
-    print(paste('on cluster ', i, ' dim of ', dim(cluster_data)))
-    for (j in 1:cluster_length-1) {
-      for (k in (j+1):cluster_length) {
-        x <- cluster_data[j,1:15]
-        y <- cluster_data[k,1:15]
-        if(dim(x) == dim(y)){
-          sum <- sum + distance(x,y)
-        }
-        
-      }
-    }
-    print(paste('done with clutser', i, ' ', sum))
-    similarities[i] = sum
-  }
-  similarities
-}
-print(graph_coesion(data_final, km$cluster, euc_dist))
-
-temp_data <- data_final
-temp_data$cluster <- km$cluster
-cluster_data <- filter(temp_data, cluster == 1)
-x <- cluster_data[1,1:15]
-y <- cluster_data[2,1:15]
-sum((x - y)^2)
-euc_dist(x,y)
-
-
-
-cluster_data
-test <- lapply(split(data_final, 1:nrow(data_final)), as.list)
-test[2][1]
-
-xValue <- 2:30
+xValue <- 2:(number_of_clusters + 1)
 for (i in xValue) {
-  km <- kmeans(data_final, centers = i, iter.max =30)
-  kms[i - 1] <- list(km)
-  scores[i - 1] <- km$tot.withinss
+  km <- kmeans(data_final, centers = i, nstart = 10, iter.max =30)
+  sse_scores[i - 1] <- km$tot.withinss
 }
-line_data <- data.frame(xValue,scores)
-ggplot(line_data, aes(x=xValue, y=scores)) +
+
+length(sse_scores)
+length(xValue)
+
+chosen_number_of_clusters <- 15
+line_data <- data.frame(xValue,sse_scores)
+p <- ggplot(line_data, aes(x=xValue, y=sse_scores)) +
   geom_line(size=1, alpha=0.9, linetype=1) +
-  ggtitle("SSE Scores") + xlab("Number of Clusters")
+  ggtitle("SSE Scores") + 
+  xlab("Number of Clusters") +
+  ylab("SSE Score") + 
+  geom_vline(xintercept = chosen_number_of_clusters, linetype="dashed", 
+               color = "red", size=1.5) + 
+  geom_text(aes(x=chosen_number_of_clusters, label="15 clusters", 
+                y=20000), colour="black", angle=90, vjust = 1.2)
+p
+
+#here we are choosing the clustering based on the SSE graph over time
+chosen_number_of_clusters <- 15
+km <- kmeans(data_final, centers = chosen_number_of_clusters, nstart = 10, iter.max =30)
+data_final$cluster <- km$cluster
+
+#add the deaths and the cases data back in 
+data_final$deaths_per100 <- data$deaths_per1000 * 10
+data_final$confirmed_cases_per1000 <- data$confirmed_cases_per1000
+
+p <- ggplot(pivot_longer(data_final, cols = c(confirmed_cases_per1000), names_to = "feature"),
+       aes(x = value, y = feature, fill = cluster)) +
+  geom_bar(stat = "identity") +
+  facet_grid(rows = vars(cluster))
+p
 
 
-km$totss
-km$tot.withinss
+1
